f340ae7a6aaf218f30446c2e6476ea1f31b88d4c
mmaeB <sanchezsangay3345@gmail.com>
2026-05-20 22:48:48 -0500

    5to commit

--- conexion.php
+++ conexion.php
@@ -1,6 +1,13 @@
 <?php
-$conexion = new mysqli("localhost", "root", "", "tienda_motos");
-if ($conexion->connect_error) {
-    die("Error de conexión: " . $conexion->connect_error);
+$host = "mysql.railway.internal";
+$user = "root";
+$pass = "IsNJWDRhiCGZnmDjRSjZUvIOkYxDqfEG";
+$dbname = "railway";
+$port = 3306;
+
+$conn = new mysqli($host, $user, $pass, $dbname, $port);
+
+if ($conn->connect_error) {
+    die("Error de conexión: " . $conn->connect_error);
 }
 ?>
